8ccf536f8c18957afac5ad9842407332d48113be
Alejandro Zuleta <alejandro@yaskapp.com>
2019-03-02 21:04:02 -0500

    Added Api method

--- Api.js
+++ Api.js
@@ -1,8 +1,25 @@
+const StockApiClient = require('./lib/StockApiClient');
 
 var Api = {
   stock: {
-    get: function(id){
-
+    /**
+     * GET /stock/{symbol}
+     * @param symbol Stock ticker symbol
+     */
+    get: (symbol) => {
+      return new Promise((resolve, reject) => {
+        const client = new StockApiClient(symbol);
+        Promise.all([
+          client.fetchLatestPrice(),
+          client.fetchLogoUrl(),
+          client.fetchLatestNewsUrl()
+        ])
+        .then(results => {
+          const [latestPrice, logoUrl, newsUrl] = results;
+          resolve({latestPrice, logoUrl, newsUrl});
+        })
+        .catch(reject);
+      });
     }
   }
 };
